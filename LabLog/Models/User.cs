46d7c8ca1f61ca3a29a256b52d202c6f2b9401a0
AlfredWeddborn <vargasleda382@gmail.com>
2026-05-29 23:33:08 -0600

    Tu mensaje de commit

--- LabLog/Models/User.cs
+++ LabLog/Models/User.cs
@@ -1,6 +1,22 @@
-﻿namespace LabLog.Models
+﻿namespace LabLog.DTOs
 {
-    public class User
+    /
+    public class RegisterDto
     {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string DisplayName { get; set; } = string.Empty;
     }
+    public class RegisterResponseDto
+    {
+        public string IdToken { get; set; } = string.Empty;
+        public string LocalId { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+    }
+
+
+
+
+
+    
 }
