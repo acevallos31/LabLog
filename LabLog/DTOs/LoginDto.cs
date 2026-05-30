ee6515e35e54b8adf8137faefc9136c0f6157710
AlfredWeddborn <vargasleda382@gmail.com>
2026-05-29 21:50:59 -0600

    Feat: loginDto

--- LabLog/DTOs/LoginDto.cs
+++ LabLog/DTOs/LoginDto.cs
@@ -2,5 +2,14 @@
 {
     public class LoginDto
     {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+
+    public class LoginResponseDto
+    {
+        public string IdToken { get; set; } = string.Empty;
+        public string LocalId { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
     }
 }
